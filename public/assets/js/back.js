0dca9e4f8265a061519441087270116a9fcff075
hitotonazo <yamamotojumpei66@gmail.com>
2026-03-06 23:28:46 +0900

    Update back.js

--- public/assets/js/back.js
+++ public/assets/js/back.js
@@ -187,7 +187,7 @@ async function initBack() {
     if (profKey) profileImg.src = resolveAssetUrl(config, profKey);
   }
 
-  // Posts A
+  // Posts AA
   const allPosts = await loadPosts();
   const posts = (allPosts || [])
     .filter(p => (p.type || 'front') === 'back')
